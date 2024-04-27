192cecb977ff37be9ea239c3a3bb8edf123fd075
TvSpichkin <88217122+TvSpichkin@users.noreply.github.com>
2024-04-28 02:55:22 +0300

    Add files via upload

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,71 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+    var value1 = 100, k = 0.5, value2 = k*value1, type1 = "RUB", type2 = "USD", info, names;
+    
+    function changeValue1() {
+        value1 = Math.round(100*value2/k)/100;
+    }
+    function changeValue2() {
+        value2 = Math.round(100*k*value1)/100;
+    }
+    function changeK() {
+        k = info.rates[type2]/info.rates[type1];
+        changeValue2();
+    }
+    
+    async function getInfo() {
+        const res = await fetch('https://open.er-api.com/v6/latest/RUB');
+
+        if(res.ok) {
+            info = await res.json();
+            names = Object.keys(info.rates);
+            changeK();
+            
+            return 1;
+        } else {
+            throw new Error('Запрос не выполнен');
+        }
+    }
+    
+    let promise = getInfo();
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    {#await promise}
+        <p>Ожидание получения актуальной информации</p>
+    {:then}
+        <label>
+            <p>Не имей</p>
+            <input type="text" bind:value={value1} on:input={changeValue2} />
+            <select bind:value={type1} on:change={changeK}>
+                {#each names as name}
+                    <option value={name}>
+                        {name}
+                    </option>
+                {/each}
+            </select>
+        </label>
+        <label>
+            <p>А имей</p>
+            <input type="text" bind:value={value2} on:input={changeValue1} />
+            <select bind:value={type2} on:change={changeK}>
+                {#each names as name}
+                    <option value={name}>
+                        {name}
+                    </option>
+                {/each}
+            </select>
+        </label>
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
+    <a id='ref' href="https://www.exchangerate-api.com">Курсы по API обменного курса</a>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    #ref {
+        font-size: 10px;
+        position: absolute;
+        right: 10px;
+        bottom: 5px;
+    }
 </style>
