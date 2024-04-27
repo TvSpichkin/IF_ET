5bb331162ef591a24aef69436ae05b070df708f0
TvSpichkin <88217122+TvSpichkin@users.noreply.github.com>
2024-04-28 02:53:48 +0300

    Add files via upload

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,21 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+var decoded; // Объявление копии массива для расшифровки
 
+function decoder() {
+    decoded = JSON.parse(JSON.stringify(encoded)); // Создание копии массива для расшифровки
+    let trueKeys = Object.keys(decoded[0]).filter(s => (s.slice(-2).toLowerCase() == 'id')&&(s != 'groupId')), // Создание массива для нужных ключей структуры
+    allIds = Object.keys(translations), // Создание массива всех идентификаторов
+    uniIds = []; // Создание массива уникальных идентификаторов
+    
+    for(let i = 0; i < decoded.length; i++) for(let j = 0; j < trueKeys.length; j++) if(allIds.indexOf(decoded[i][trueKeys[j]]) != -1) {
+        if(uniIds.indexOf(decoded[i][trueKeys[j]]) == -1) uniIds.push(decoded[i][trueKeys[j]]); // Заполнение массива уникальных идентификаторов
+        decoded[i][trueKeys[j]] = translations[decoded[i][trueKeys[j]]]; // Расшифровка полей
+    }
+    
+    console.log(uniIds);
+}
 
-// console.log(decoded)
+decoder();
+console.log(decoded)
